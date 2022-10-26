a917eedeb9a02d2bf13507d23bb4e64ebc648802
Salman Saif <salmanthedev@gmail.com>
2022-10-26 23:33:00 +0500

    Update settings.py

--- src/explorify/settings.py
+++ src/explorify/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'h$$0+m$8erydgzj^%6_11j6bsl#3+6(^-ks*=e6^e1n$#0k8jo'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
